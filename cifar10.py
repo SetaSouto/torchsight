d606c0a7d7a5511db1e5ca1f0e70d4984b008c01
Fabián Souto <fab.souto@gmail.com>
2018-09-14 15:17:15 -0300

    Refactoring of the callbacks in the trainer.
    Now it has its own methods that you could extend.
    
    cifar10.py is an example.

--- cifar10.py
+++ cifar10.py
@@ -1,4 +1,6 @@
-"""Module to fine-tune a resnet architecture on the CIFAR10 dataset"""
+"""Module to fine-tune a resnet architecture on the CIFAR10 dataset.
+This is an example on how it could be used the Trainer class to train a model.
+"""
 import os
 
 import torch
@@ -8,90 +10,117 @@ from models.resnet import Resnet
 from trainer import Trainer
 
 
-def main():
-    """Main function to train a resnet module on the CIFAR10 dataset."""
-    data_root = './data'
-    if not os.path.exists('./logs'):
-        os.mkdir('./logs')
-    logs_dir = './logs/cifar10'
-    num_classes = 10
-    image_size = 32
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model_type = 18  # Resnet18
-    print_every = 100  # Print info every 100 batches
-    # Hyperparameters
-    batch_size = 64
-    epochs = 10
-    learning_rate = 1e-3
-    momentum = 0.9
-    shuffle = True
-    num_workers = 2
+class Cifar10Trainer(Trainer):
+    """Custom trainer for a network based on resnet trained over CIFAR10 dataset."""
 
-    # Transformation for the dataset
-    transformations = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    # Datasets and data loaders
-    trainset = torchvision.datasets.CIFAR10(root=data_root, train=True, download=True,
-                                            transform=transformations)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=shuffle,
-                                               num_workers=num_workers)
-    validset = torchvision.datasets.CIFAR10(root=data_root, train=False, download=True,
-                                            transform=transformations)
-    valid_loader = torch.utils.data.DataLoader(validset, batch_size=batch_size, shuffle=False,
-                                               num_workers=num_workers)
-    # Get the model, criterion and optimizer
-    model = Resnet(type=model_type, num_classes=num_classes,
-                   image_size=image_size)
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(
-        model.parameters(), lr=learning_rate, momentum=momentum)
-    # Initialize the trainer
-    trainer = Trainer(model=model, train_loader=train_loader, criterion=criterion,
-                      optimizer=optimizer, device=device, logs_dir=logs_dir)
+    def __init__(self, *args, hyperparameters=None, model_type=18, **kwargs):
+        """Initialize the trainer. It needs the hyperparameters for the training.
+        The hyperparameters is a dict with these keys:
+            - batch_size (int)
+            - epochs (int)
+            - learning_rate (float)
+            - momentum (float)
+            - weight_decay (float)
+            - shuffle (bool)
+            - num_workers (int)
+        It has default values for each key, see implementation.
+        """
+        # Set the hyperparameters for the training
+        default_hyperparameters = {
+            "batch_size": 64,
+            "epochs": 10,
+            "learning_rate": 1e-4,
+            "momentum": 0.9,
+            "weight_decay": 5e-4,
+            "shuffle": True,
+            "num_workers": 2
+        }
+        if hyperparameters:
+            self.hyperparameters = {**default_hyperparameters, **hyperparameters}
+        else:
+            self.hyperparameters = default_hyperparameters
+        # Set the logs dir
+        data_root = './data'
+        if not os.path.exists('./logs'):
+            os.mkdir('./logs')
+        logs_dir = './logs/cifar10'
+        # Set the parameters for this dataset
+        num_classes = 10
+        image_size = 32
+        # Transformation for the dataset
+        transformations = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        # Datasets and data loaders
+        trainset = torchvision.datasets.CIFAR10(root=data_root, train=True, download=True,
+                                                transform=transformations)
+        train_loader = torch.utils.data.DataLoader(trainset,
+                                                   batch_size=self.hyperparameters["batch_size"],
+                                                   shuffle=self.hyperparameters["shuffle"],
+                                                   num_workers=self.hyperparameters["num_workers"])
+        validset = torchvision.datasets.CIFAR10(root=data_root, train=False, download=True,
+                                                transform=transformations)
+        valid_loader = torch.utils.data.DataLoader(validset,
+                                                   batch_size=self.hyperparameters["batch_size"],
+                                                   shuffle=False,
+                                                   num_workers=self.hyperparameters["num_workers"])
+        # Get the model, criterion and optimizer
+        model = Resnet(type=model_type, num_classes=num_classes,
+                       image_size=image_size)
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(),
+                                    lr=self.hyperparameters["learning_rate"],
+                                    momentum=self.hyperparameters["momentum"],
+                                    weight_decay=self.hyperparameters["weight_decay"])
+        # Initialize the trainer
+        description = "\n".join(["{}: {}".format(key, value)
+                                 for key, value in self.hyperparameters.items()])
+        super().__init__(model=model,
+                         train_loader=train_loader,
+                         criterion=criterion,
+                         optimizer=optimizer,
+                         logs_dir=logs_dir,
+                         description=description,
+                         valid_loader=valid_loader,
+                         *args, **kwargs)
+        # Parameters for the callbacks
+        self.batches_total_loss = 0
+        self.batches_total_time = 0
+        self.print_every_batches = 100
 
-    def validset_precision():
-        """Calculates the precision over the validset."""
+    def train(self):
+        """Train the model using the epochs given in the hyperparameters."""
+        super().train(self.hyperparameters["epochs"])
+
+    def batch_callback(self, epoch, index, batch_time, loss):
+        """Prints some accumulated statistics every some batches."""
+        self.batches_total_loss += loss
+        self.batches_total_time += batch_time
+        if index % self.print_every_batches == self.print_every_batches - 1:
+            mean_batch_loss = self.batches_total_loss / self.print_every_batches
+            print("[{}\t{}]\tTotal time: {}\tMean loss: {}".format(
+                epoch + 1, index + 1, self.batches_total_time, mean_batch_loss))
+            self.batches_total_loss = 0
+            self.batches_total_time = 0
+
+    def epoch_callback(self, epoch, epoch_time, loss):
+        """Prints the precision over the validation dataset."""
+        # Re initialize the stats for the next batches
+        self.batches_total_loss = 0
+        self.batches_total_time = 0
         correct = 0
         total = 0
         with torch.no_grad():
-            for (inputs, labels) in valid_loader:
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
+            for (inputs, labels) in self.valid_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-        print('Accuracy of the network on the 10000 test images: {}%'.format(
-            100 * correct / total))
-
-    class BatchCallback:
-        """A class to keep the state of the batch callback."""
-
-        def __init__(self):
-            """Initialize the total loss."""
-            self.total_loss = 0
-            self.total_time = 0
-
-        def get_callback(self):
-            """Function to call every time after each batch process.
-            It prints some statistics."""
-            def function(epoch, batch_index, batch_time, loss):
-                self.total_loss += loss
-                self.total_time += batch_time
-                if batch_index % print_every == print_every - 1:
-                    mean_batch_loss = self.total_loss / print_every
-                    print("[{}\t{}]\tTotal time: {}\tMean loss: {}".format(
-                        epoch + 1, batch_index + 1, self.total_time, mean_batch_loss))
-                    self.total_loss = 0
-                    self.total_time = 0
-            return function
-
-    # TRAINING
-    trainer.train(epochs=epochs, epoch_callback=validset_precision,
-                  batch_callback=BatchCallback().get_callback())
-
+        print('[Epoch: {}\tTime: {}\tLoss: {}] Accuracy of the network on the 10000 test images: {}%'.format(
+            epoch + 1, epoch_time, loss, 100 * correct / total))
 
 if __name__ == "__main__":
-    main()
+    Cifar10Trainer().train()
--- trainer.py
+++ trainer.py
@@ -4,6 +4,9 @@ import time
 import numpy as np
 import torch
 
+# TODO: Real time visualization of the loss.
+# TODO: Smart saving of the weights.
+
 
 class Trainer:
     """A class to abstract the logic of the training. It trains the model with the given
@@ -11,13 +14,16 @@ class Trainer:
     options.
     """
 
-    def __init__(self, model, train_loader, criterion, optimizer, device=None, valid_loader=None,
-                 logs_dir=None):
+    def __init__(self, model, train_loader, criterion, optimizer, logs_dir=None, description=None, device=None, valid_loader=None):
         """Initialize a trainer to train and/or evaluate a model.
 
         The loaders must return a tuple as (input, target, *more) where more could be more arguments
         but this class only use the first element of the tuple as the input for the model and the
         second argument as the target output.
+
+        The description of the trainer could be useful to save metadata of the training as the
+        hyperparameters, conditions or anything related to the training itself or other things.
+        The description is saved inside the logs directory.
 
         Args:
             model (torch.nn.Module): The model created with pytorch to train.
@@ -32,6 +38,9 @@ class Trainer:
             optimizer (torch.optim.Optimizer): The optimizer of the training.
                 (E.g. Stochastic Gradient Descent).
                 For the optimizer package see: https://pytorch.org/docs/stable/optim.html
+            logs_dir (str, optional): Path to the directory where to save the logs of the training.
+            description (str): Description of the training. Useful to save metadata related to the
+                training as hyperparameters, conditions and other information.
             device (str): String indicating the device where to run the training.
                 If cuda is available it automatically set the device to cuda.
                 See: https://pytorch.org/docs/stable/tensor_attributes.html#torch.torch.device
@@ -39,7 +48,6 @@ class Trainer:
                 set.
                 This is useful to keep the track of the loss over the validation set.
                 The results are tracked in the log file. Or you can call the valid() method.
-            logs_dir (str, optional): Path to the directory where to save the logs of the training.
         """
         self.model = model
         self.train_loader = train_loader
@@ -47,64 +55,66 @@ class Trainer:
         self.optimizer = optimizer
         self.device = device if device else 'cuda:0' if torch.cuda.is_available() else 'cpu'
         self.valid_loader = valid_loader
-        self.start_time = None  # start time of the training
+        self.timestamp = time.time()
         if logs_dir:
+            # Set the logs directory and create it if it does not exists
             self.logs_dir = os.path.abspath(logs_dir)
             if not os.path.exists(self.logs_dir):
                 os.makedirs(self.logs_dir)
+            # Save the description of the trainer
+            description_path = "{}/{}_description.txt".format(self.logs_dir, self.timestamp)
+            with open(description_path, 'w') as file:
+                file.write(description)
 
-    def train(self, epochs=1, epoch_callback=None, batch_callback=None):
+    def train(self, epochs=1):
         """Train the model for the given epochs.
+
+        It calls the the epoch_callback and batch_callback methods. This is useful
+        if you want to add more code to this method so you can print some stats or
+        whatever you want. You must extend this trainer and implement those methods.
 
         Args:
             epochs (int): Number of epochs to run.
                 An epoch is a full pass over all the images of the dataset.
-            epoch_callback (function): Function call every time that an epoch
-                finishes.
-            batch_callback (function): A function that is called with the arguments
-                of the log method (without the name, that is obviously 'train')
-                after each batch process. See log() for documentation.
         """
         # Move the model to the device and set to train mode, useful for batch normalization or
-        # dropouts modules.
-        # For more info see:
+        # dropouts modules. For more info see:
         # https://discuss.pytorch.org/t/trying-to-understand-the-meaning-of-model-train-and-model-eval/20158
         self.model.to(self.device)
         self.model.train()
-
-        self.start_time = time.time()
 
         print('Training started.')
         if self.logs_dir:
             print('Logs can be found at {}'.format(self.logs_dir))
 
         for epoch in range(epochs):
-            batch_start_time = time.time()
+            epoch_start_time = time.time()
+            last_batch_end_time = time.time()
+            epoch_loss = 0
             for i, (inputs, targets, *_) in enumerate(self.train_loader):
-                # Get input, targets and train
-                inputs, targets = inputs.to(
-                    self.device), targets.to(self.device)
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                # Optimize
                 self.optimizer.zero_grad()
                 outputs = self.model(inputs)
                 loss = self.criterion(outputs, targets)
                 loss.backward()
                 self.optimizer.step()
                 # Log the batch
-                batch_time = time.time() - batch_start_time
-                # We need to put here the the batch start time to get the time
-                # of loading the data
-                batch_start_time = time.time()
-                self.log(epoch, i, batch_time, loss, 'train')
-                # Batch callbacks
-                if batch_callback:
-                    batch_callback(epoch, i, batch_time, loss)
+                batch_time = time.time() - last_batch_end_time
+                # TODO: Investigate why changing the way that it measures the time
+                # changes the result. If we move the last_batch_end_time to the end
+                # it does not measures the batch time well
+                last_batch_end_time = time.time()
+                self.log(epoch, i, batch_time, loss)
+                # Increment epoch loss
+                epoch_loss += loss
+                # Batch callback
+                self.batch_callback(epoch, i, batch_time, loss)
             # Epoch callback
-            if epoch_callback:
-                epoch_callback()
-            # Validate one time per epoch
-            self.validate(epoch)
+            epoch_time = time.time() - epoch_start_time
+            self.epoch_callback(epoch, epoch_time, epoch_loss)
 
-    def log(self, epoch, batch_index, batch_time, loss, name):
+    def log(self, epoch, batch_index, batch_time, loss, train=True):
         """Append to the given log's name at the logs_dir directory a new log line.
 
         It logs the data as:
@@ -116,7 +126,7 @@ class Trainer:
             - batch time: How many time take that batch to evaluate.
             - loss: The computed loss for the given batch.
 
-        The log file is saved as indicated in the documentation of _get_log_file_path.
+        The log file is saved as indicated in the documentation of get_log_file_path.
 
         Args:
             epoch (int): The epoch that was running when the loss was calculated.
@@ -125,36 +135,41 @@ class Trainer:
                 (could be getted by time.time()).
             loss (torch.Tensor): Tensor with one dimension and one item that represents the loss
                 for the given epoch and batch.
-            name (str): Name of the log. Useful to set the file's name.
+            train (bool): Indicates if it is logging the train set or not. If it is false,
+                the method assumes that is logging the validation set.
         """
         if self.logs_dir:
-            file_path = self._get_log_file_path(name)
+            file_path = self.get_log_file_path(
+                'train' if train else 'validation')
             with open(file_path, 'a') as file:
                 file.write('{} {} {} {} {}\n'.format(
                     time.time(), epoch, batch_index, batch_time, loss.item()))
 
-    def _get_log_file_path(self, name):
-        """Returns the log's file path as <self.logs_dir>/logs_<name>_<self.start_time>.txt"""
-        return os.path.join(self.logs_dir, 'logs_{}_{}.txt'.format(name, self.start_time))
+    def get_log_file_path(self, dataset):
+        """Returns the log's file path as <self.logs_dir>/<self.timestamp>_logs_<dataset>.txt
 
-    def load_logs(self, name):
+        Args:
+            dataset (str): Indicates the dataset that is logging.
+        """
+        return os.path.join(self.logs_dir, '{}_logs_{}.txt'.format(self.timestamp, dataset))
+
+    def load_logs(self, dataset):
         """Returns a tensor with shape (number of logs, 5) containing all the logs for the given
-        log name.
+        dataset.
 
         Returns:
             torch.Tensor: The tensor containing all the logs.
         """
-        file_path = self._get_log_file_path(name)
+        file_path = self.get_log_file_path(dataset)
         return self.load_logs_from(file_path)
 
-    @staticmethod
-    def load_logs_from(path):
+    def load_logs_from(self, path):
         """Load the logs tensor stored at path.
 
         Returns:
             torch.Tensor: The tensor containing all the logs.
         """
-        return torch.from_numpy(np.loadtxt(path))
+        return torch.from_numpy(np.loadtxt(path)).to(self.device)
 
     def validate(self, epoch):
         """Validate the model to the given valid dataset.
@@ -173,3 +188,29 @@ class Trainer:
             # Log the validation
             self.log(epoch, 0, time.time() - start_time,
                      total_loss, 'validation')
+
+    def batch_callback(self, epoch, index, batch_time, loss):
+        """Callback to call every ending of a batch processing.
+
+        Useful for add more code to the training as print statistics or whatever
+        you want to execute at the end of the batch processing.
+
+        Args:
+            epoch (int): Index of the epoch that is running.
+            index (int): Index of the batch that is running.
+            batch_time (float): How much time (in seconds) did the batch take to be processed.
+            loss (torch.Tensor): Tensor with one item as the total loss of the batch.
+        """
+        pass
+
+    def epoch_callback(self, epoch, epoch_time, loss):
+        """Callback to call every ending of a epoch processing.
+
+         Useful to execute custom code after each epoch.
+
+         Args:
+            epoch (int): Index of the epoch that run.
+            epoch_time (float): Total time of the epoch in seconds.
+            loss (torch.Tensor): Tensor with one item that is the total loss of the epoch.
+        """
+        pass
