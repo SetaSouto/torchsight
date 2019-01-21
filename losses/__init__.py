8312eb55a2887980d16acdb94dc9f439e5a9855c
Fabián Souto <fab.souto@gmail.com>
2019-01-21 16:44:49 -0300

    Add classes and functions to init packages to get more beautiful imports.

--- losses/__init__.py
+++ losses/__init__.py
@@ -5,6 +5,4 @@ Import directly to perform more beautiful imports.
 See:
 http://mikegrouchy.com/blog/2012/05/be-pythonic-__init__py.html
 """
-from .anchors import Anchors
-from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152, ResNet
-from .retinanet import RetinaNet
+from .focal import FocalLoss
