3409e30ca73d69ad6c9f193c761ab6a777df7a93
Fabián Souto <fab.souto@gmail.com>
2019-03-07 12:21:54 -0300

    Add convergence test to focal loss.

--- test/losses/focal.py
+++ test/losses/focal.py
@@ -31,9 +31,30 @@ class TestFocalLoss(unittest.TestCase):
         self.assertEqual(0., float(classification))
         self.assertEqual(0., float(regression))
 
-    def test_correct(self):
-        """Test that with correct annotations gives zero loss."""
-        # Simulate a batch with size 1, 5 classes, 3 annotations and 5 anchors for an image of size 450x450
+    def get_loss_arguments(self):
+        """Return the arguments for the loss.
+
+        Simulate a batch with size 1, 5 classes and 3 annotations for an image of size 450x450.
+
+        It returns the anchors, regressions, classifications and annotations for a fake image.
+        The classification and the regressions are the "perfect" ones, it must be the minimum loss,
+        in theory, zero, but as we clamp the values to avoid NaN it must be a value near zero.
+
+        Returns:
+            torch.Tensor: The base anchors for the image.
+                Shape:
+                    (total anchors, 4)
+            torch.Tensor: The regression values to adjust the base anchors.
+                Shape:
+                    (total anchors, 4)
+            torch.Tensor: The classifications probabilities for each class. Each classification value
+                has the correct value, i.e. has all the values as 0 but no the correct one that is 1.
+                Shape:
+                    (total anchors, 5)
+            torch.Tensor: The fake annotations for the fake image.
+                Shape:
+                    (3, 5)
+        """
         annotations = torch.Tensor([[0., 0., 100., 100., 3.],
                                     [150., 150., 350., 350., 2.],
                                     [400., 400., 450., 450., 1.]]).to(self.device)
@@ -63,21 +84,58 @@ class TestFocalLoss(unittest.TestCase):
         classifications = torch.zeros((anchors.shape[0], 5)).to(self.device)
         selected_anchors = iou > self.loss.iou_object  # Anchors below that threshold are background
         classifications[selected_anchors, assigned_annotations[selected_anchors, -1].long()] = 1.
-        # Get the losses (unsqueeze to add the batch dimension)
-        classification_loss, regression_loss = self.loss(anchors.unsqueeze(0),
-                                                         regressions.unsqueeze(0),
-                                                         classifications.unsqueeze(0),
-                                                         annotations.unsqueeze(0))
+
+        # Unsqueeze to simulate the batch size of 1
+        return (tensor.unsqueeze(0) for tensor in [anchors, regressions, classifications, annotations])
+
+    def test_correct(self):
+        """Test that with correct annotations gives zero loss."""
+        arguments = self.get_loss_arguments()
+        classification_loss, regression_loss = self.loss(*arguments)
         self.assertEqual(0., float(regression_loss))
-        self.assertLess(abs(0. - float(classification_loss)), 1e20)
-        # Change the labels to turn on all the other classes but not the correct one
-        classifications = 1 - classifications
-        classification_loss, _ = self.loss(anchors.unsqueeze(0),
-                                           regressions.unsqueeze(0),
-                                           classifications.unsqueeze(0),
-                                           annotations.unsqueeze(0))
-        self.assertGreater(float(classification_loss), 43.)
-        # TODO: Test with no regression values (box == anchor) and with correct label.
+        classification_loss = float(classification_loss)
+        self.assertLess(abs(0. - classification_loss), 1e20)
+
+    def test_convergence(self):
+        """Test that the loss has the structure to take the classification values to the correct one
+        and it does not have a local minimum.
+
+        How can we do this? By taking the perfect classification and increasing the classification score
+        by a little value to all the other non-correct labels. The loss always must increase.
+        """
+        anchors, regressions, classifications, annotations = self.get_loss_arguments()
+        # Take the MAXIMUM value that the loss could be theoretically by changing all the labels.
+        # Change the labels to turn on all the other labels but not the correct one
+        classifications_test = 1 - classifications
+        classification_loss, _ = self.loss(anchors, regressions, classifications_test, annotations)
+        classification_loss = float(classification_loss)
+        self.assertGreater(classification_loss, 43.)
+        # Iterate increasing the bad labels
+        increase_by = 0.1
+        last_loss = float(self.loss(anchors, regressions, classifications, annotations)[0])
+        mask = classifications != 1
+        classifications_test = classifications.clone()
+        for _ in range(int(1 / increase_by)):
+            classifications_test[mask] += increase_by
+            loss = float(self.loss(anchors, regressions, classifications_test, annotations)[0])
+            current_prob = float(classifications_test[mask][0])
+            print('[Current prob {:.7f}] [Last {:.7f}] [Actual {:.7f}]'.format(current_prob, last_loss, loss))
+            self.assertLess(last_loss, loss)
+            last_loss = loss
+
+        # Check if the loss decreases if the correct ones increases from zero to one
+        print('------------------')
+        mask = classifications == 1
+        classifications_test = classifications.clone()
+        classifications_test[mask] = 0
+        last_loss = float(self.loss(anchors, regressions, classifications_test, annotations)[0])
+        for _ in range(int(1 / increase_by)):
+            classifications_test[mask] += increase_by
+            loss = float(self.loss(anchors, regressions, classifications_test, annotations)[0])
+            current_prob = float(classifications_test[mask][0])
+            print('[Current prob {:.7f}] [Last {:.7f}] [Actual {:.7f}]'.format(current_prob, last_loss, loss))
+            self.assertGreater(last_loss, loss)
+            last_loss = loss
 
 
 if __name__ == '__main__':
