0c970354cdaaaccd9f647b01778f1dae99443e2b
Fabián Souto <fab.souto@gmail.com>
2019-03-12 16:30:57 -0300

    Modify train script to use it as a CLI.

--- train.py
+++ train.py
@@ -1,12 +1,33 @@
-"""Script to show how to use a Trainer."""
+"""Script to run a trainer.
+
+You can modify the ResNet backbone and the batch size. For other hyperparameters modifications
+you can edit the hyperparameters argument of the trainer. To see all the hyperparameters please
+visit the original class definition.
+"""
+import argparse
+
 from torchsight.trainers import RetinaNetTrainer
 
-RetinaNetTrainer(
-    hyperparameters={
-        'RetinaNet': {'classes': 1, 'resnet': 18},
-        'datasets': {'class_names': ('person',), 'train': 'val2017', 'validation': 'val2017'},
-        'optimizer': {'learning_rate': 1e-5}
-    },
-    logs='./logs/retinanet',
-    checkpoint='./logs/retinanet/1548616852/checkpoint_epoch_3.pth.tar'
-).train(validate=False)
+PARSER = argparse.ArgumentParser(description='Train a RetinaNet model using COCO dataset.')
+PARSER.add_argument('model', help='The model to train. Currently only supported "RetinaNet".', default='RetinaNet')
+PARSER.add_argument('-r', '--root', help='Root directory of the COCO dataset (cocoapi).', required=True)
+PARSER.add_argument('-b', '--batch-size', help='Batch size', default=8)
+PARSER.add_argument('--resnet', help='ResNet backbone to use.', default=50)
+PARSER.add_argument('-l', '--logs-dir', help='Directory where to save the logs and checkpoints.',
+                    default='./logs/retinanet')
+PARSER.add_argument('-c', '--checkpoint', help='Absolute path to the checkpoint to continue an old training')
+
+ARGUMENTS = PARSER.parse_args()
+
+if ARGUMENTS.model.lower() == 'retinanet':
+    RetinaNetTrainer(
+        hyperparameters={
+            'RetinaNet': {'resnet': int(ARGUMENTS.resnet)},
+            'datasets': {'root': ARGUMENTS.root},
+            'dataloaders': {'batch_size': int(ARGUMENTS.batch_size)}
+        },
+        logs_dir=ARGUMENTS.logs_dir,
+        checkpoint=ARGUMENTS.checkpoint
+    ).train()
+else:
+    raise ValueError('The model "{}" is not supported.'.format(ARGUMENTS.model))
