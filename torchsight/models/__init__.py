61172aad08ebb482b55ffbed0d6f31ff506e71c5
Fabián Souto <fab.souto@gmail.com>
2019-03-20 16:04:49 -0300

    Add DLDENet to __init__ in models package.

--- torchsight/models/__init__.py
+++ torchsight/models/__init__.py
@@ -6,5 +6,6 @@ See:
 http://mikegrouchy.com/blog/2012/05/be-pythonic-__init__py.html
 """
 from .anchors import Anchors
-from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152, ResNet
+from .dlde import DLDENet
+from .resnet import ResNet, resnet18, resnet34, resnet50, resnet101, resnet152
 from .retinanet import RetinaNet
