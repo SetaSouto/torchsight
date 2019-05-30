ae1cc4ec90bac784b74d73c3f1db3df2ba0cb807
Fabián Souto <fab.souto@gmail.com>
2019-05-30 16:16:43 -0400

    Adapt visualization of the logo32plus dataset with the resize transform.

--- torchsight/cli/visualize/logo32plus.py
+++ torchsight/cli/visualize/logo32plus.py
@@ -1,9 +1,5 @@
 """Visualize the dataset Logo32plus."""
-import random
-
 import click
-
-from torchsight.datasets import Logo32plusDataset
 
 
 @click.command()
@@ -13,7 +9,15 @@ from torchsight.datasets import Logo32plusDataset
 def logo32plus(dataset_root, dataset, no_shuffle):
     """Visualize the images and annotations of the Logo32plus dataset that has its root directory
     at DATASET-ROOT."""
-    dataset = Logo32plusDataset(dataset_root, dataset)
+    import random
+    from torchvision.transforms import Compose
+    from torchsight.datasets import Logo32plusDataset
+    from torchsight.transforms.detection import Resize
+
+    dataset = Logo32plusDataset(dataset_root, dataset, transform=Compose([
+        Resize(min_side=384, max_side=512)
+    ]))
+
     length = len(dataset)
     print('Dataset length: {}'.format(length))
     indexes = list(range(length))
