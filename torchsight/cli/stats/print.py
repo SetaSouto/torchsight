912919f5f49d74fbf573bbe6c5d6f100116a266d
Fabián Souto <fab.souto@gmail.com>
2019-06-20 18:58:57 -0400

    Improve the stats prints.

--- torchsight/cli/stats/print.py
+++ torchsight/cli/stats/print.py
@@ -6,9 +6,10 @@ from torchsight.loggers import PrintLogger
 
 @click.command()
 @click.argument('log_file')
-@click.option('-lk', '--loss-key', default='Loss', help='The key in the logs that indicates the loss of the batch.')
+@click.option('-k', '--keys', default='Loss Class. Regr. Simil. w-norm LR', show_default=True)
 @click.option('-ek', '--epoch-key', default='Epoch', help='The key in the logs that indicates the epoch.')
-def printlogger(log_file, loss_key, epoch_key):
+@click.option('--just', default=12, show_default=True)
+def printlogger(log_file, keys, epoch_key, just):
     """Get the mean loss per epoch over the training dataset and validation dataset.
 
     This stats could be generated from the logs that generated the PrintLogger and were
@@ -36,21 +37,51 @@ def printlogger(log_file, loss_key, epoch_key):
 
     Where the `---` means that there is no data yet to show.
     """
-    logger = PrintLogger(description=None)
-    train = logger.epochs_losses(filepath=log_file, epoch_key=epoch_key,
-                                 loss_key=loss_key, keep=lambda x: x[:10] == '[Training]')
-    valid = logger.epochs_losses(filepath=log_file, epoch_key=epoch_key,
-                                 loss_key=loss_key, keep=lambda x: x[:12] == '[Validating]')
-    epochs = train.keys()
+    logger = PrintLogger()
+    keys = keys.split()
 
-    padding = 15
-    print('{} | {} | {}'.format('EPOCH'.rjust(padding // 3),
-                                'TRAIN LOSS'.rjust(padding),
-                                'VALIDATION LOSS'.rjust(padding)))
-    for epoch in epochs:
-        train_loss = '{:.7f}'.format(float(train[epoch]['average'])).rjust(padding)
-        try:
-            valid_loss = '{:.7f}'.format(float(valid[epoch]['average'])).rjust(padding)
-        except KeyError:
-            valid_loss = '---'
-        print('{} | {} | {}'.format('{}'.format(epoch).rjust(padding // 3), train_loss, valid_loss))
+    def accumulate(logs):
+        epochs = {}
+
+        for log in logs:
+            epoch = log[epoch_key]
+
+            if epoch not in epochs:
+                epochs[epoch] = {}
+
+            for k in keys:
+                if k not in epochs[epoch]:
+                    epochs[epoch][k] = {'sum': 0, 'count': 0}
+
+                value = log.get(k, None)
+                epochs[epoch][k]['sum'] += float(value) if value is not None else 0
+                epochs[epoch][k]['count'] += 1 if value is not None else 0
+
+        return epochs
+
+    train = accumulate(logger.read(log_file, keep=lambda x: x[:10] == '[Training]'))
+    valid = accumulate(logger.read(log_file, keep=lambda x: x[:12] == '[Validating]'))
+
+    headers = ['Epoch']
+    for key in keys:
+        headers += [key.center(just * 2 + 1)]
+
+    print(' | '.join(headers))
+
+    for epoch in train:
+        values = [str(epoch).center(5)]
+
+        for k in keys:
+            if train[epoch][k]['count'] == 0:
+                train_value = '---'.rjust(just)
+            else:
+                train_value = '{:.7f}'.format(train[epoch][k]['sum'] / train[epoch][k]['count']).rjust(just)
+
+            if valid.get(epoch, None) is None or valid[epoch].get(k, None) is None or valid[epoch][k]['count'] == 0:
+                valid_value = '---'.rjust(just)
+            else:
+                valid_value = '{:.7f}'.format(valid[epoch][k]['sum'] / valid[epoch][k]['count']).rjust(just)
+
+            values.append('{} {}'.format(train_value, valid_value))
+
+        print(' | '.join(values))
