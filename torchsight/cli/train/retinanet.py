68c9464d81c9a6ce2fe07a3e064910d53d2d6558
Fabián Souto <fab.souto@gmail.com>
2019-06-27 20:29:02 -0400

    Make not required the logs dir in the command to train the retinanet.

--- torchsight/cli/train/retinanet.py
+++ torchsight/cli/train/retinanet.py
@@ -9,7 +9,7 @@ import click
 @click.option('-dr', '--dataset-root', type=click.Path(exists=True))
 @click.option('-b', '--batch-size', default=8, show_default=True)
 @click.option('--resnet', default=50, show_default=True, help='The resnet backbone that the model must use.')
-@click.option('--logs-dir', default='./logs', type=click.Path(exists=True), show_default=True,
+@click.option('--logs-dir', default='.', type=click.Path(), show_default=True,
               help='Where to store the checkpoints and descriptions.')
 @click.option('-c', '--checkpoint', type=click.Path(exists=True), help='A checkpoint to resume the training from it.')
 @click.option('--classes', default=None,
