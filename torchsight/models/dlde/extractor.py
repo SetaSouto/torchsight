02878c51885af4be27e1e2033236601966bce040
Fabián Souto <fab.souto@gmail.com>
2019-07-01 18:05:59 -0400

    Add a DLDENetExtractor to get the embeddings and bounding boxes for a batch of images.

---
+++ torchsight/models/dlde/extractor.py
@@ -0,0 +1,30 @@
+"""Module with the feature extractor version of the DLDENet."""
+import torch
+
+from .weighted import DLDENet
+
+
+class DLDENetExtractor(DLDENet):
+    """A model to get embeddings with bounding boxes instead or class proabilities."""
+
+    def forward(self, images):
+        """Generate the embeddings and bounding boxes for them for the given images.
+
+        Arguments:
+            images (torch.Tensor): of the batch with shape `(batch size, 3, height, width)`.
+
+        Returns:
+            torch.Tensor: with the embeddings. Shape `(batch size, num of embeddings, embedding dim)`.
+            torch.Tensor: with the bounding boxes for the embeddings.
+                Shape `(batch size, num of embeddings, 4)` with the `x1, y1, x2, y2` for the top-left
+                corner and the bottom-right corner of the box.
+        """
+        with torch.no_grad():
+            feature_maps = self.fpn(images)
+            regressions = torch.cat([self.regression(feature_map) for feature_map in feature_maps], dim=1)
+            embeddings = torch.cat([self.encode(feature_map) for feature_map in feature_maps], dim=1)
+            anchors = self.anchors(images)
+            bounding_boxes = self.anchors.transform(anchors, regressions)
+            bounding_boxes = self.anchors.clip(images, bounding_boxes)
+
+            return embeddings, bounding_boxes
