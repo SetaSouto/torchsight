6eb45874f35e7bc9208f1c45a3ec58a643a85ac5
Fabián Souto <fab.souto@gmail.com>
2019-07-11 11:19:02 -0400

    Change command to visualize the flickr32 dataset with only some brands.

--- torchsight/cli/visualize/flickr32.py
+++ torchsight/cli/visualize/flickr32.py
@@ -7,21 +7,23 @@ import click
               help='The root directory of the dataset.')
 @click.option('--dataset', default='trainval', type=click.Choice(['training', 'validation', 'trainval', 'test']))
 @click.option('--no-shuffle', is_flag=True, help='Show the images in order and not randomly.')
-@click.option('--classes', help='Visualize only this classes. Ex: "google esso"')
+@click.option('--brands', help='Visualize only this classes. Ex: "google esso"')
 @click.option('--only-boxes', is_flag=True, help='Visualize only images with bounding boxes.')
-def flickr32(dataset_root, dataset, no_shuffle, classes, only_boxes):
+def flickr32(dataset_root, dataset, no_shuffle, brands, only_boxes):
     """Visualize the images and annotations of the Flickr32 dataset that has its root directory
     at DATASET-ROOT."""
     import random
     from torchsight.datasets import Flickr32Dataset
     from torchsight.transforms.augmentation import AugmentDetection
 
-    dataset = Flickr32Dataset(dataset_root, dataset, transform=AugmentDetection(evaluation=True, normalize=False),
-                              classes=classes, only_boxes=only_boxes)
+    dataset = Flickr32Dataset(
+        root=dataset_root, dataset=dataset, transform=AugmentDetection(evaluation=True, normalize=False),
+        brands=brands, only_boxes=only_boxes
+    )
 
     length = len(dataset)
     print('Dataset length: {}'.format(length))
-    print('Classes: {}'.format(list(dataset.class_to_label.keys())))
+    print('Brands: {}'.format(dataset.brands))
     indexes = list(range(length))
 
     if not no_shuffle:
